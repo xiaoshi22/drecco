00f3d98c56c1e0ec364834426a5076b8d065b9ce
Xiaoshi Wang <xiaoshiwang22@gmail.com>
2020-11-28 16:28:45 -0500

    added the br

--- games/GuessWhatItIs/index.php
+++ games/GuessWhatItIs/index.php
@@ -28,7 +28,6 @@
         <h1 id="gameName">Guess What It Is</h1>
         <h3 id="groupName">Winner Team</h3>
         <h3>Instruction:</h3>
-        <h3>Instruction:</h3>
         <div id="gameDesc" class="jumbotron">
             <strong>Overview:</strong> <br/>
             <p>Initially, we have cards labeled from 1 to 2n + 1. Player A and B both take 
@@ -46,27 +45,27 @@
                 wins.
             </p>
             <strong>Example:</strong> <br/>
-                The interesting part is that palyer A could ask player B if B has a card that A has. 
-                For example, we have cards 1, 2, 3, 4, 5
-                A: 1, 2
-                B: 3, 4
-                5 is left on the table
-
-                1st turn:
-                A: "do you have 1?"
-                B: "no"
-
-                2nd turn:
-                B: "do you have 2?"
-                A: "yes"
-
-                3rd turn:
-                A: "do you have 4?"
-                B: "yes"
-
-                4st turn:
-                B: "5 is on the table!"
-
+                The interesting part is that palyer A could ask player B if B has a card that A has. <br>
+                For example, we have cards 1, 2, 3, 4, 5 <br>
+                A: 1, 2 <br>
+                B: 3, 4 <br>
+                5 is left on the table <br>
+                <br> 
+                1st turn: <br>
+                A: "do you have 1?" <br>
+                B: "no"<br>
+                <br>
+                2nd turn:<br>
+                B: "do you have 2?"<br>
+                A: "yes"<br>
+                <br>
+                3rd turn:<br>
+                A: "do you have 4?"<br>
+                B: "yes"<br>
+                <br>
+                4st turn:<br>
+                B: "5 is on the table!"<br>
+                <br>
                 Because B has 3 and 4, so B know 1, 2 and 3 could be left. And After A asks 1 after the first turn, A did not claim 1 is on the table on the third turn. B knows 1 is on A asked a card he has. So, he knows A has 1 and 2. Thus, B is confident to say 5 is left :)
         </div>
         <h3>Leaderboard:</h3>
