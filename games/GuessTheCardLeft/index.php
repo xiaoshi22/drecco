91b2a342381a8cd8e64e669a27b5f548bb7f5679
Xiaoshi Wang <xiaoshiwang22@gmail.com>
2020-11-30 18:20:23 -0500

    renamed again

--- games/GuessTheCardLeft/index.php
+++ games/GuessTheCardLeft/index.php
@@ -78,7 +78,7 @@
 	    *       if smaller score is better. Otherwise no need to pass variable
 	    *       
 	    */
-	    getScore("GuessTheCardOnTheTable", 0);
+	    getScore("GuessTheCardLeft", 0);
 	?>
     </div>
         <h3> Play game in pop up window:</h3>
@@ -87,7 +87,7 @@
     <?php include $base."footer.php"; ?>
 </div>
 <script type="text/javascript">
-    newWindowBtn(800,800,"games/GuessTheCardOnTheTable/iframe.html", []);
+    newWindowBtn(800,800,"games/GuessTheCardLeft/iframe.html", []);
 </script>
 </body>
 </html>
